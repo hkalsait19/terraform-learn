590c03b057139f7f101c7a3afb9daebedf9bd460
hkalsait19 <harshavardhan.kalsait19@gmail.com>
2024-06-29 20:27:50 +0530

    added central-variable.tf
    added central-variable.tf for supporting terraform-variables.tf file due to we protect values inside variable.tf file

---
+++ terraform-variable/central-variable.tf
@@ -0,0 +1,19 @@
+variable "vpn_ip" {
+  default = "10.20.30.40/32"
+}
+
+variable "ip_protocol" {
+  default = "tcp"
+}
+
+variable "ssh_port" {
+  default = "22"
+}
+
+variable "ftp_port" {
+  default = "21"
+}
+
+variable "app_port" {
+  default = "8080"
+}
